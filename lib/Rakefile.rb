fa24467fc8e49338f1cbf2175c5af1c7fd083cde
leiby <leiby@b2eec21e-c2bc-4cec-aa62-991c620a4ccf>
2007-09-15 03:37:03 +0000

    empty netbeans project setup
    git-svn-id: svn+ssh://rubyforge.org/var/svn/gallery-remote@1 b2eec21e-c2bc-4cec-aa62-991c620a4ccf

---
+++ lib/Rakefile.rb
@@ -0,0 +1,12 @@
+# 
+# Rakefile.rb
+# 
+# Created on Sep 14, 2007, 11:17:42 PM
+# 
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+ 
+
+require 'rake'
+require 'rake/testtask'
+require 'rake/rdoctask'
